f2e5034b69fce69ba17451864f21e55b67ae1a5a
Shantanu <om1232050@gmail.com>
2024-11-19 23:27:21 +0530

    Update greeting.py

--- PSET1/greeting.py
+++ PSET1/greeting.py
@@ -1,2 +1,8 @@
-name=(input("Enter your name!:  ")).capitalize()
-print("Hey there !",name)
+greeting = input("Greetings user! ")
+greeting_def = greeting.lower().replace(" ","")
+if greeting_def == "hello":
+    print("0$")
+elif greeting_def[0]=="h":
+    print("20$")
+else :
+    print("100$")
