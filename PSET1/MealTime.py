44f4dbd92dae76a1e3213b99f038b77e017f0d1e
Shantanu <om1232050@gmail.com>
2024-11-19 23:26:16 +0530

    Update MealTime.py

--- PSET1/MealTime.py
+++ PSET1/MealTime.py
@@ -1,11 +1,19 @@
-time=int(input("Enter the time:  "))
-if time>=6 and time<=10 :
-    print("It's breakfast time!")
-elif time>10 and time<=14:
-    print("It's lunch time!")
-elif time >14 and time <= 18:
-    print("It's snacks time!🍴")
-elif time>18 and time <20:
-    print("It's dinner time")
-else :
-    print("it's no time to eat🙁")
+def main():
+    time=input("Enter the time: ")
+#creating a variable to recall the function for 24 hr meal time
+    meal=convert(time)
+    print(meal)
+    if meal>=7 and meal <= 8 :
+        print("It's time for breakfast")
+    elif meal >= 12 and meal <= 13 :
+        print("it's time for lunch")
+    elif meal >= 18 and meal <= 19 :
+        print("It's dinner time")
+    else: print("it's no time to eat")
+def convert(time):
+#defining the function for the 24 hr meal time
+    hour,min = time.split(":")
+    hour = float(hour) + (float(min)/60)
+    return hour
+if __name__=="__main__":
+    main()
